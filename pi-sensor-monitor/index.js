0a2ae4bc79490ec702c38033287f6255f74795c7
git <kontakt@bartoszzawadka.pl>
2016-04-12 22:00:06 +0200

    Added postgres support (PG) and ORM (Sequelize). Prepared databases and db user accounts

--- pi-sensor-monitor/index.js
+++ pi-sensor-monitor/index.js
@@ -2,9 +2,12 @@
  * Created by barte_000 on 2016-04-11.
  */
 var server = require('./server');
+var models = require('./models');
 
 var port = Number(process.env.SERVER_PORT) ||  8080;
 
-server.listen(port, function(){
-   console.log('PiSensorMonitor listening on %j', server.address());
+models.sequelize.sync().then(function(){
+   server.listen(port, function(){
+      console.log('PiSensorMonitor listening on %j', server.address());
+   });
 });
